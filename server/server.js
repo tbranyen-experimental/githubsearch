0b0b5cdbc445493ea4170fc00e321f0779b78a9d
Alex Navasardyan <alex.navasardyan@yahoo.com>
2012-10-17 22:50:33 -0400

    chaning index.html and enhancing server to understand app, assets directories, adding npmignore

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-var search = require('octonode'),
-    _      = require('underscore'),
-    fs      = require('fs'),
-    server = require('express').createServer();
+var search          = require('octonode'),
+    express         = require('express'),
+    _               = require('underscore'),
+    fs              = require('fs'),
+    server          = require('express').createServer(),
+    staticDirectory = express['static'];
 
 module.exports = function (options) {
     options = _.extend({
@@ -9,6 +11,16 @@ module.exports = function (options) {
         tests: true,
         baseDir: './'
     }, options || {});
+
+    server.configure(function () {
+        ['app', 'assets', 'tests'].forEach(function (directory) {
+            server.use(
+                    '/' + directory,
+                    staticDirectory(options.baseDir + directory)
+            );
+        });
+        server.use(express.bodyParser());
+    });
 
     server.get('/', function (request, response) {
         fs.createReadStream(options.baseDir + 'app/index.html').pipe(response);
