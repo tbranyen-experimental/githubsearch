1858fa943353f550685aff5bcc54bd104ae73b77
Alex Navasardyan <alex.navasardyan@yahoo.com>
2012-10-17 22:17:02 -0400

    enhancing nodejs module & adding dev server

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+var search = require('octonode'),
+    _      = require('underscore'),
+    fs      = require('fs'),
+    server = require('express').createServer();
+
+module.exports = function (options) {
+    options = _.extend({
+        port: 9999,
+        tests: true,
+        baseDir: './'
+    }, options || {});
+
+    server.get('/', function (request, response) {
+        fs.createReadStream(options.baseDir + 'index.html').pipe(response);
+    });
+
+    server.listen(options.port || null);
+    console.log("Running at http://localhost:" + (options.port || ''));
+};
